d0c2f696db6b936d5bb3a7a1e474137ed39bb130
Ram Muthukumaran <RMuthukumaran@imo-online.com>
2019-05-30 10:23:48 -0500

    added cors

--- Intern/Startup.cs
+++ Intern/Startup.cs
@@ -25,23 +25,32 @@ namespace Intern
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
+            // Add swagger documentation
             services.AddSwaggerDocument(config =>
             {
                 config.Title = Configuration.GetValue<string>("SwaggerApplicationTitle");
                 config.Description = Configuration.GetValue<string>("SwaggerDescription");
                 config.Version = Configuration.GetValue<string>("SwaggerDocumentVersion");
-            }
-            );
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                p => p.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials());
+            });
 
             services.AddSingleton<IDatabaseService, PostgreSQLService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -50,12 +59,15 @@ namespace Intern
             }
             else
             {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
+            app.UseCors("AllowAll");
+
             app.UseSwagger();
             app.UseSwaggerUi3();
-            
+
             app.UseHttpsRedirection();
             app.UseMvc();
         }
